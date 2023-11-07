8435dc3fd49fd76d69170c2c06372462ade92ec8
LeslieAmaya <leslieamayal001@gmail.com>
2023-11-07 07:55:24 -0700

    Agregar archivos de proyecto.

---
+++ EjercicioPrototype/Casa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPrototype
+{
+    public class Casa :ICloneable
+    {
+        public int NumExt { get; set; }
+        public string? Calle { get; set; }
+        public int NumHabitaciones { get; set; }
+        public string? Extras { get; set; }
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ EjercicioPrototype/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EjercicioPrototype
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Casa casaold = new Casa()
+            {
+                NumExt = 123,
+                Calle = "Calle", 
+                NumHabitaciones = 3,
+                Extras = "Alberca"
+            };
+            Casa casanew = casaold.Clone() as Casa;
+            casanew.Extras = "Albercas y balcon";
+
+            Console.WriteLine(casaold.NumExt);
+            Console.WriteLine(casaold.Calle);
+            Console.WriteLine(casaold.NumHabitaciones);
+            Console.WriteLine(casaold.Extras);
+            Console.WriteLine("----------------------");
+            Console.WriteLine(casanew.NumExt);
+            Console.WriteLine(casanew.Calle);
+            Console.WriteLine(casanew.NumHabitaciones);
+            Console.WriteLine(casanew.Extras);
+        }
+    }
+}
